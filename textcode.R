c709a908274be83a57225ad5fbd9b92dbb551e52
Priyesh Mehta <mehtapriyesh7401@gmail.com>
2020-08-19 17:55:37 +0530

    Final Update!

--- textcode.R
+++ textcode.R
@@ -4,10 +4,12 @@ library(SnowballC)
 library(tidyverse)
 library(tidytext)
 library(RColorBrewer)
+library(ggrepel)
 
-x = 7 #top n number of participants to be analysed
 
-chat <- read_delim("path", "]", escape_double = FALSE,
+path = "C:/Users/user/Desktop/_chat.txt"
+
+chat <- read_delim(path, "]", escape_double = FALSE,
                      col_names = FALSE, trim_ws = TRUE) %>%
   select(-X1) %>%
   separate(
@@ -15,42 +17,46 @@ chat <- read_delim("path", "]", escape_double = FALSE,
   ) %>%
   filter(!is.na(sender))
   mutate(
+    sender = str_squish(sender),
     said = str_to_lower(said)
   ) %>%
     mutate(
-      sender = fct_lump(sender, x)
+      sender = fct_lump(sender, n)
     ) 
   
 
-#Alternate text processing strategy. Depends on the type of your text file.
-# chat <- read_csv("path", col_names = FALSE) %>%
+#Alternate text processor. Depends on the type of your text file.
+# chat <- read_csv(path, col_names = FALSE) %>%
 #   separate(X2, into = c("X3", "X4"), sep = 10) %>%
 #   separate(X4, into = c("sender", "said"), sep = ":") %>%
 #   select(sender, said) %>%
 #   mutate(
-#     said = str_to_lower(said)
+#     said = str_to_lower(said),
+#     sender = str_squish(sender)
 #   ) %>%
 #   filter(!is.na(sender)) %>%
 #   mutate(
-#     sender = fct_lump(sender, x)
-#   ) 
+#     sender = fct_lump(sender, n)
+#   )
 
 
-#Most active participant.
-chat %>%
+n = 7
+#MOst active participant.
+q = chat %>%
   count(sender, sort = T)  %>%
   mutate(sender = fct_reorder(sender,n)) %>%
   ggplot(aes(sender, n, fill=sender))+
   geom_bar(stat = "identity")+ 
-  coord_flip()
+  coord_flip() + 
+  labs(x = "Sender", y = "# of messages sent", title = "Most active participants in the group") + 
+  scale_fill_manual(values = brewer.pal(8, "Spectral")) 
+
 
 chart = chat%>%
   unnest_tokens(words, said) %>% 
   #In case you want to club stemmed words.
   #mutate(words = wordStem(words)) %>% 
   filter (!words %in% c(stop_words$word, "omitted", "media", "image", "deleted"), str_detect(words, "^[a-z]"), !is.na(words)) 
-
-
 
 # top words that account for around 15% of all the words that were sent.
 top_words = chart %>%
@@ -60,22 +66,39 @@ top_words = chart %>%
   filter(quartile <= 0.15)
 
 
-chart %>%
+data = chart %>%
   filter(words %in% top_words$words) %>%
   group_by(words, sender) %>%
   count() %>%
   ungroup() %>%
   group_by(words) %>%
-  mutate(count = sum(n)) %>%
+  mutate(count = sum(n),
+         pct = (round(n/count,2))) %>%
+  arrange(count, desc(sender)) %>%
+  mutate(
+    cumu = cumsum(n),
+    y.pos = lag(cumu, default = 0) + n*0.5 
+  ) %>%
   ungroup() %>%
   mutate(
-    words = fct_reorder(words, count)  
-  ) %>%
-  ggplot(aes(words, n)) +
-  geom_bar(stat = "identity", aes(fill = sender)) +
-  coord_flip() + 
-  scale_fill_manual(values = brewer.pal(x+1, "Spectral")) +
-  labs(y = "# of messages sent", x = "Most used words", title = "Top 15% Words categorised by senders", subtitle = "Top words calculated using most frequent words arranged by their percentile.")
-  
-  
- 
+    words = fct_reorder(words, count)
+  )
+
+# filtering data for considerably large percentage points.
+filt = data %>%
+  filter(pct>0.15)
+
+g = ggplot(NULL, aes(words, n)) +
+    geom_bar(data = data, stat = "identity", aes(fill = sender)) +
+    coord_flip() + 
+    geom_text(data = filt, aes(x = words, label = scales::percent(pct), y = y.pos)) + 
+    scale_fill_manual(values = brewer.pal(8, "Spectral")) +
+    labs(y = "# of messages sent", x = "Most used words", title = "Top 15% Words categorised by senders", subtitle = "Top words calculated using most frequent words arranged by their percentile.")
+
+
+print(q)
+print(g)
+
+
+
+
